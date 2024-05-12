c24937c52db923221f64300ed24e6273ef0839ab
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2024-05-12 13:58:36 +0800

    added cardsList

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,49 @@
 import React, { useState } from "react";
 import Header from "./Header";
 
+const cardsList = [
+  {
+    id: 1,
+    name: "barbeque",
+    photoName: "foods/barbeque.jpg",
+  },
+  {
+    id: 2,
+    name: "carbonara",
+    photoName: "foods/carbonara.jpg",
+  },
+  {
+    id: 3,
+    name: "graham",
+    photoName: "foods/graham.jpg",
+  },
+  {
+    id: 4,
+    name: "shawarma",
+    photoName: "foods/shawarma.jpg",
+  },
+  {
+    id: 5,
+    name: "shake",
+    photoName: "foods/shake.jpg",
+  },
+  {
+    id: 6,
+    name: "ramen",
+    photoName: "foods/ramen.jpeg",
+  },
+  {
+    id: 7,
+    name: "spaghetti",
+    photoName: "foods/spaghetti.jpg",
+  },
+  {
+    id: 8,
+    name: "icecream",
+    photoName: "foods/icecream.jpg",
+  },
+];
+
 export default function App() {
   return (
     <div className="container">
