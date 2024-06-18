3039f3b598163bf826b3af677966561214f1996d
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2024-06-18 21:58:58 +0800

    made a copy of each element then shuffle the order

--- src/components/CardsGrid.js
+++ src/components/CardsGrid.js
@@ -1,17 +1,30 @@
-import React, { useState } from "react";
-// import Anime from "./Anime";
+import React, { useState, useEffect } from "react";
 
 export default function CardsGrid({ cardsList }) {
-  const cards = cardsList;
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    // Double the content of the cards array
+    const doubledCards = [...cardsList, ...cardsList];
+
+    // Shuffle the array using the Fisher-Yates algorithm
+    for (let i = doubledCards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [doubledCards[i], doubledCards[j]] = [doubledCards[j], doubledCards[i]];
+    }
+
+    setCards(doubledCards);
+  }, [cardsList]);
+
   const numCards = cards.length;
 
   return (
     <main className="menu">
       {numCards > 0 ? (
         <ul className="cards">
-          {/* here we iterate each data on an object and used the data as props to render components for each data */}
-          {cards.map((card) => (
-            <li className={`card`} key={card.name}>
+          {/* Here we iterate over each card and use the data as props to render components for each card */}
+          {cards.map((card, index) => (
+            <li className={`card`} key={`${card.name}-${index}`}>
               <img src={card.photoName} alt={card.name} title="Flip" />
             </li>
           ))}
