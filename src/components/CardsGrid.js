c92d17ddf67d3ad6d23ecf3e42263e748f457ce3
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2024-07-28 21:42:58 +0800

    added selectedCard state and flipCard func

--- src/components/CardsGrid.js
+++ src/components/CardsGrid.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 export default function CardsGrid({ cardsList }) {
   const [cards, setCards] = useState([]);
+  const [selectedCard, setSelectedCard] = useState([]);
 
   useEffect(() => {
     // Double the content of the cards array
@@ -16,6 +17,10 @@ export default function CardsGrid({ cardsList }) {
     setCards(doubledCards);
   }, [cardsList]);
 
+  function flipCard(card, index){
+
+  }
+
   const numCards = cards.length;
 
   return (
@@ -24,8 +29,8 @@ export default function CardsGrid({ cardsList }) {
         <ul className="cards">
           {/* Here we iterate over each card and use the data as props to render components for each card */}
           {cards.map((card, index) => (
-            <li className={`card`} key={`${card.name}-${index}`}>
-              <img className={`card_img_hide`} src={card.photoName} alt={card.name} title="Flip" />
+            <li className={`card`} key={`${card.name}-${index}`} onClick={(e) => flipCard(card, index)}>
+              <img className={`card_img_hide`} src={card.photoName} alt={`${card.name}-${index}`} title="Flip" />
             </li>
           ))}
         </ul>
